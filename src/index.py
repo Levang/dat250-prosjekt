3c0538d9ba62857a9bf471be478f6e68de78835b
Ardijan <ardijanrx@gmail.com>
2020-09-14 14:56:07 +0200

    Fikset import

--- src/index.py
+++ src/index.py
@@ -1,11 +1,18 @@
+# import sys
+# sys.path.insert(0, '/pages')    #Dersom import ikke fungerer fjern kommentar
+# ─── Linjene over tillater import som vanlig fra pages mappen ────────────────────────────────────────────────────────────────────────
 from flask import Flask, render_template
+from pages import overview 
 
 app = Flask(__name__)
 
-
-@app.route("/login/")
+@app.route("/")
 def login():
     return render_template("login.html")
+
+@app.route("/overview")
+def overviewpage():
+    return overview.hello_overview()
 
 
 if __name__ == '__main__':
