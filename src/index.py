96d7c6323ad78ec2525634cc87e12a879b683006
Joachim Andreassen <jo.andreassen@stud.uis.no>
2020-09-14 15:08:38 +0200

    Added some css and html to index page

--- src/index.py
+++ src/index.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/login/")
-def login():
-    return render_template("login.html")
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
