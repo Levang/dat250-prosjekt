00611ec3a1dc998554bfeca15e08a12be3a3c264
Levang <lars.levang@hotmail.com>
2020-09-16 11:03:29 +0200

    new file structure, basic database functionality

--- src/app.py
+++ src/app.py
@@ -1,21 +1,8 @@
 # import sys
 # sys.path.insert(0, '/pages')    #Dersom import ikke fungerer fjern kommentar
 # ─── Linjene over tillater import som vanlig fra pages mappen ────────────────────────────────────────────────────────────────────────
-from flask import Flask, render_template
-from pages import overview 
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-  
-
-@app.route("/overview")
-def overviewpage():
-    return overview.hello_overview()
-
+from safecoin import app
 
 if __name__ == '__main__':
     app.run(debug=True)
