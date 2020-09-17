950d35b2c419c077224f7d9a6ae301852a4d9c24
Levang <lars.levang@hotmail.com>
2020-09-17 10:33:34 +0200

    fixes

--- src/safecoin/models.py
+++ src/safecoin/models.py
@@ -5,11 +5,7 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-<<<<<<< HEAD
 class User(db.Model, UserMixin):
-=======
-class User(db.Model):
->>>>>>> 4b5c0c9e72be70ee1b00659b11035b7d09d3a269
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False, primary_key=True)
     password = db.Column(db.String(60), nullable=False)
