62f56d2d1b8e6c3c1240e98c4dc9b56e5050822d
Ardijan <ardijanrx@gmail.com>
2020-09-18 17:41:08 +0200

    Finished frontpage login and register

--- src/safecoin/forms.py
+++ src/safecoin/forms.py
@@ -6,14 +6,19 @@ from safecoin.models import User
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "email@example.com"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "password"})
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "password"})
     submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('This email address is already in use.')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "email@example.com"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "password"})
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
--- src/safecoin/pages.py
+++ src/safecoin/pages.py
@@ -4,10 +4,12 @@ from safecoin import app
 
 # --- Main page --- #
 @app.route("/")
-def home():
+def homePage():
     return render_template("index.html")
 
-
+@app.route("/login")
+def loginPage():
+    return render_template("login.html")
 #app.route('/overview')
 #ef helloOverview():
 #   return 'Hello from overview'
