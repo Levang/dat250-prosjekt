56463ccbf7a664c57fb09b2d7ab60a44728d8f27
ardijanr <a.rexhaj@stud.uis.no>
2020-10-17 15:16:05 +0200

    Lagrer mellomarbeid

--- safecoin/safecoin/register.py
+++ safecoin/safecoin/register.py
@@ -57,68 +57,81 @@ def register():
     form = RegistrationForm()
     form2 = TwoFactorAuthRegForm()
 
+
     # form er den første formen som er på /register, som per nå bare inneholder email, passord og en submit-knapp
 
-    # form2 er den andre formen du kommer til etter du submitter "form". Denne nye siden vil da inneholde QR-koden 
-    # for å legge 2fa-nøkkelen inn i din valgte 2fa app. Denne siden har også et passord felt, 2fa felt (for koden du nå kan generere i appen), 
+    # form2 er den andre formen du kommer til etter du submitter "form". Denne nye siden vil da inneholde QR-koden
+    # for å legge 2fa-nøkkelen inn i din valgte 2fa app. Denne siden har også et passord felt, 2fa felt (for koden du nå kan generere i appen),
     # og et "read-only" som inneholder eposten du skrev inn på forrige side.
 
     if form.validate_on_submit():
         errList = []
         getPasswordViolations(errList, form.password.data)
 
+        #Is there any error in the generated information
         if len(errList) == 0:
+
+            # ─── HASHED EMAIL IS USER ID ─────────────────────────────────────
             hashed_email = flask_scrypt.generate_password_hash(form.email.data, "")
-            print(hashed_email)
 
-            salt = flask_scrypt.generate_random_salt()
-            print(salt)
-
-            hashed_pw = flask_scrypt.generate_password_hash(form.password.data, salt)
-            print(hashed_pw)
-
+            # ─── CHECK IF THE EMAIL EXISTS IN DATABASE ───────────────────────
             if User.query.filter_by(email=hashed_email.decode("utf-8")).first():
                 flash("error")
                 return render_template("register.html", form=form)
 
-            #print(f"encryption key: {enKey}")
-            # ─── TESTACCOUNTS ───────────────────────────────────────────────────────────────
-            # ─── ADDS ACCOUNTS TO NEW USER REPLACED WHEN CREATE ACCOUT IS WORKING ───────────
-            accountlist=[11112248371,11112239950,11112235147,11112205956,11112262143,11112270258,11112294379,11112250314,11112293269,11112278435,11112208700]
-            accountName=['bob','Alot','savings','expences','toiletMoney','company1','company2','wifey','daughter','son','grandchild','brother','theTeapot','Games','gambling']
-            # ─── TESTACCOUNTS ───────────────────────────────────────────────────────────────
+            # ─── IF THE USER DOES NOT EXIST IN THE DATABASE ──────────────────
+            # Create a user dictionairy for redis.
+            userDict = {}
 
-            encryptedKey=encrypt(form.password.data,'generate',True) # generate new encrypted key with users password
-            deKey=decrypt(form.password.data,encryptedKey,True)      # decrypt the key
-            mailEncrypted=encrypt(deKey,form.email.data)             # encrypt the email
+            # add hashed email as key to cleartext email
+            # This can be directly saved to database later as user email
+            userDict['email'] = hashed_email.decode("utf-8")
 
-            # ─── TESTING ACCOUNTS ───────────────────────────────────────────────────────────
-            # ─── ADDS ACCOUNTS TO NEW USER REPLACED WHEN CREATE ACCOUT IS WORKING ───────────
-            accountsString=''
-            for i in range(len(accountlist)):
-                accountsString=accountsString + (f'{accountName[i]},{accountlist[i]},privatekey{i};')
-            # ─── TESTING ACCOUNTS ───────────────────────────────────────────────────────────
+            #We need to temporarily keep the users email in plaintext while in redis
 
-            accountsEnc=encrypt(deKey,accountsString)
+            userDict["PlainEmail"] = form.email.data.decode("utf-8")
 
-            print(f' decrytion key {deKey}')
-            print(f' decryted email {decrypt(deKey,mailEncrypted)}')
+            # ─── SALT AND HASH PASSWORD ──────────────────────────────────────
+            salt = flask_scrypt.generate_random_salt()
+
+            # add hashed password to user dictionairy
+            # This can be directly saved to database later
+            userDict['password'] = flask_scrypt.generate_password_hash(form.password.data, salt) + salt
+
+            # ─── GENERATE USER ENCRYPTION KEY ────────────────────────────────
+
+            # generate new encrypted key with users password
+            encryptedKey=encrypt(form.password.data,'generate',True)
+
+            # decrypt the key again, serves as a double check
+            deKey=decrypt(form.password.data,encryptedKey,True)
+
+            # No if deKey, explicitly show what you are testing this against.
+            if deKey!=None:
+                userDict['enKey'] = encryptedKey
 
 
+            # encrypt the email and add it to userDict
+            userDict['enEmail'] = encrypt(deKey,form.email.data)
 
             # ─── TESTING 2-FACTOR AUTHENTICATION ───────────────────────────────────────────────────#
 
-            secret_key = pyotp.random_base32()  #Lager en relativt simpel secret_key, men det virker. Har kompatibilitet med Google Authenticator.
-            qr_link = pyotp.totp.TOTP(secret_key).provisioning_uri(name=form.email.data, issuer_name="Safecoin.tech") #Denne genererer QR-koden
+            #Lager en relativt simpel secret_key. Har kompatibilitet med Google Authenticator.
+            secret_key = pyotp.random_base32()
+
+            #denne maa tas vare på til neste side, krypteres med kundes passord
+            userDict['2faKey'] = encrypt(deKey,secret_key)
+
+            #Genererer link for kundes qr kode
+            qr_link = pyotp.totp.TOTP(secret_key).provisioning_uri(name=form.email.data, issuer_name="Safecoin.tech")
+
             return render_template('TwoFactor.html', form2 = form2, qr_link = qr_link) # Vi må dra med inn qr_linken for å generere qr_koden korrekt
-        
+
         for err in errList:
             flash(err, "error")
-
-
             # TODO: Legg inn en måte for å ta med infoen om brukeren (AKA alle tingene som vi må legge inn med brukeren i databasen
             # som vil si: hashed_email, enEmail, password, enKey og secretkey). Vi kan potensielt kryptere emailen med passordet, som key
-            # for redis, i den første formen. På det viset så kan alt verifiseres ettersom emailen blir dratt med automatisk over, og hvis vi bruker 
+            # for redis, i den første formen. På det viset så kan alt verifiseres ettersom emailen blir dratt med automatisk over, og hvis vi bruker
             # email kryptert med passordet (som de er nødt til å oppgi på ny uansett), så kan vi sjekke om denne stemmer med den som er som key i redis.
 
     if form2.validate_on_submit():
@@ -131,13 +144,12 @@ def register():
             totp = pyotp.totp.TOTP(secret_key) #secret_key må være den ukrypterte secret_key-en
 
             if totp.verify(form2.otp.data): # Hvis brukeren scanner qrkoden (som genereres i html) vil koden som vises i appen deres matche koden til totp.now()
-            
-            # TODO: problem her: serveren som verifiserer totp koden er ca 10 sekunder foran den koden som genereres i google authenticator. Dette gjør slik at hvis vi legger inn totp-koden rett før den resettes, så vil det ikke virke.    
-                
+
+            # TODO: problem her: serveren som verifiserer totp koden er ca 10 sekunder foran den koden som genereres i google authenticator. Dette gjør slik at hvis vi legger inn totp-koden rett før den resettes, så vil det ikke virke.
+
                 print("Iffen validerte!!")
                 print(f"  -  {form2.email.data} _ {form.password.data}")
-                user = User(email=hashed_email.decode("utf-8"), enEmail=mailEncrypted, password=(hashed_pw+salt).decode("utf-8"),enKey=encryptedKey, secret=secret_key)             #------------ Legger til secret key i database-brukeren -----------------#
-                user.accounts=accountsEnc
+                user = User(email=hashed_email.decode("utf-8"), enEmail=mailEncrypted, password=(hashed_pw+salt).decode("utf-8"),enKey=encryptedKey, secret=secret_key)
                 db.session.add(user)
                 db.session.commit()
                 flash('Your account has been created! You are now able to log in.', 'success')
