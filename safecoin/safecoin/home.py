961268f1d42756c8738cf66e92219b8365e35664
Levang <lars.levang@hotmail.com>
2020-10-17 13:16:42 +0200

    new branch for reporting

--- safecoin/safecoin/home.py
+++ safecoin/safecoin/home.py
@@ -19,8 +19,13 @@ def home():
 
         login , userDB = verifyUser(form.email.data,form.password.data,addToActive=True)
         if login:
-            login_user(userDB, remember=form.remember.data)
-            return redirect(url_for("overviewPage"))
+# ------------ 2 faktor på login -----------------#
+                                                #TODO
+            totp = pyotp.TOTP(userDB.secret)    #Må dekryptere secretkey her, AKA userDB.secret inni den totp variabelen må være den ukrypterte
+            if totp.verify(form.otp.data)       #Denne returnerer True hvis koden fra brukeren er overens med serveren. MERK: serveren sin 
+                                                #genererte totp er tilsynelatende omtrent 10 sekunder foran koden som brukeren genererer..
+                login_user(userDB, remember=form.remember.data)
+                return redirect(url_for("overviewPage"))
         else:
             flash('Wrong username or password. Please try again.')
     return render_template("login.html", form=form)
