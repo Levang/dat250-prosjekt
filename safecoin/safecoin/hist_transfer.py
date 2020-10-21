015ca5d1bee3c16ba20587863d3f1c81a8772b05
Joachim Andreassen <jo.andreassen@stud.uis.no>
2020-10-21 16:04:38 +0200

    Fixed formatting issues. Fixed BIG bug in register

--- safecoin/safecoin/hist_transfer.py
+++ safecoin/safecoin/hist_transfer.py
@@ -9,7 +9,7 @@ from flask_wtf import FlaskForm
 from safecoin import app, redis, json, db, disable_caching
 from safecoin.forms import TransHistory
 from safecoin.models import Transactions
-from safecoin.accounts_db import format_account_number
+from safecoin.accounts_db import format_account_number, format_account_balance
 from safecoin.accounts import getAccountsList, format_account_list
 
 
@@ -32,6 +32,7 @@ def transactions():
 
     return render_template('hist_transfer.html', transHistory=TransList, form=transForm), disable_caching
 
+
 def QueryToList(query,accountList,currentAccount):
 
     user_dict = json.loads(redis.get(current_user.email))
@@ -43,27 +44,26 @@ def QueryToList(query,accountList,currentAccount):
     for i in query:
         if i.accountFrom in str(accountList):
             name = user_dict['accounts'][i.accountFrom][0]
-            accountFrom=f'{name} ( {i.accountFrom} )'
+            accountFrom=f'{name} ( {format_account_number(i.accountFrom)} )'
         else:
             accountFrom = i.accountFrom
 
         if i.accountTo in str(accountList):
             name=user_dict['accounts'][i.accountTo][0]
-            accountTo=f'{name} ( {i.accountTo} )'
+            accountTo=f'{name} ( {format_account_number(i.accountTo)} )'
         else:
             accountTo = i.accountTo
 
-        tempAmount = str(i.amount)
-        amount = f'{tempAmount[:-2]},{tempAmount[-2:]}'
-        in_ = ''
-        out = ''
+        amount = format_account_balance(i.amount)
+        in_ = "──"
+        out = "──"
 
         if str(i.accountFrom) == str(currentAccount):
             out = amount
-            in_ = ''
+            in_ = "──"
 
         if str(i.accountTo) == str(currentAccount):
-            out = ''
+            out = "──"
             in_ = amount
 
         #print(f"tempamout is {type(tempAmount)}")
