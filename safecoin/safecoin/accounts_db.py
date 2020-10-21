c679befe5a29278fa1eb64b750fee45f5d84b5ed
Joachim Andreassen <jo.andreassen@stud.uis.no>
2020-10-21 10:21:04 +0200

    Fixed minor bug. Please approvve

--- safecoin/safecoin/accounts_db.py
+++ safecoin/safecoin/accounts_db.py
@@ -92,8 +92,8 @@ def illegalChar(text, maxlength):
             return True
 
 
-def addNewAccountToCurUser(password, otp, name="My account", user=None, money=False, isNotCurrentUser=False):
-    if isNotCurrentUser:
+def addNewAccountToCurUser(password, otp, name="My account", user=None, money=False, isCurrentUser=True):
+    if isCurrentUser:
         authenticated, _ = verify_pwd_2FA(password, otp)
         if not authenticated:
             return "Couldn't create account due to an error"
